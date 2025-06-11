d53be5ac25d09df8590b7a3ada9c8aa02e69b6a5
winxonn44 <winxonnguyen44@gmail.com>
2025-06-10 22:52:41 -0700

    Day 2 stuff

---
+++ variables.js
@@ -0,0 +1,29 @@
+let name = "Winxon"
+const year = 2025
+var isLearning = true;
+
+console.log(name);
+console.log(year);
+console.log(isLearning);
+
+
+let student = {
+    name: "Winxon",
+    age: 18,
+    isCool: true,
+};
+
+let birthYear = 2025 - student.age
+
+console.log(student.name,student.age,birthYear);
+
+let arrayFoods = ["sushi", "steak", "boba"];
+console.log(arrayFoods[1]);
+
+let book = {
+    title: "John Snow",
+    author: "Johnny Snow White",
+    yearPublished: 2029
+};
+
+console.log(typeof[book])
